ff9e3b946e0eb73e7cf9c70a503308f5507c95dd
Denilson Fodor <Denilson.fodor@newtechs.com.br>
2024-05-10 16:01:05 -0300

    colunas adto

--- src/app/adto/adto.component.ts
+++ src/app/adto/adto.component.ts
@@ -10,30 +10,27 @@ import { PoBreadcrumb, PoPageAction, PoTableColumn } from '@po-ui/ng-components'
   styleUrl: './adto.component.css'
 })
 export class AdtoComponent implements OnInit {
-  actions: Array <PoPageAction> = [
-    {
-      label:'Detalhe',
-      url:'adto/detalhe',
-    },
+  colunas: Array<PoTableColumn> = 
+  [
+    {property: 'codigo_documento',    label: 'CODIGO',      type: 'string'},
+    {property: 'tipo',                label: 'TIPO',        type: 'string'},
+    {property: 'cpf_cnpj',            label: 'CPF/CNPJ',    type: 'string'},
+    {property: 'data_emissao',        label: 'EMISSÃO',     type: 'date'  },
+    {property: 'data_vencimento',     label: 'VENCIMENTO',  type: 'date'  },
+    {property: 'valor_documento',     label: 'VALOR',       type: 'string'},
+    {property: 'observação',          label: 'OBSERVAÇÃO',  type: 'string'},
+    {property: 'cod_forma_pagto',     label: 'FORMA PAGTO', type: 'string'},
+    {property: 'conta_contabil',      label: 'CTA.CONTABIL',type: 'string'},
+    {property: 'natureza',            label: 'NATUREZA',    type: 'string'},
+    {property: 'centro_custo',        label: 'C.CUSTO',     type: 'string'},
+    {property: 'banco_transf',        label: 'C.CUSTO',     type: 'string'},
+    {property: 'agencia_transf',      label: 'C.CUSTO',     type: 'string'},
+    {property: 'cta_corrente_transf', label: 'C.CUSTO',     type: 'string'},
   ];
 
-  breadcrumb: PoBreadcrumb = {
-    items: [{label: 'Home', 
-             link: '/adto', 
-             label:'Adiantamento'}],
-    },
-    items: any;
-
-    colunas: Array <PoTableColumn> = [
-      {property: ,
-       label: ,
-       type
-      }
-    ]
-  }
   constructor() {}
 
   ngOnInit(): void {
     
   }
-}
+ }
