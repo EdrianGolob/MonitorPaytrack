ec494b558aaa0cfdc6797df4ca2896c7e319ebcd
Denilson Fodor <Denilson.fodor@newtechs.com.br>
2024-05-13 22:55:38 -0300

    filtros

--- src/app/adto/adto.component.ts
+++ src/app/adto/adto.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { PoBreadcrumb, PoPageAction, PoTableColumn } from '@po-ui/ng-components';
+import { PoModalComponent, PoTableColumn } from '@po-ui/ng-components';
+import { PoNotificationService } from '@po-ui/ng-components';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-adto',
@@ -10,6 +12,9 @@ import { PoBreadcrumb, PoPageAction, PoTableColumn } from '@po-ui/ng-components'
   styleUrl: './adto.component.css'
 })
 export class AdtoComponent implements OnInit {
+  //@ViewChild('optionsForm', { static: true }) form: NgForm;
+  //@ViewChild(PoModalComponent, { static: true}) poModal: PoModalComponent
+
   colunas: Array<PoTableColumn> = 
   [
     {property: 'codigo_documento',    label: 'CODIGO',      type: 'string'},
@@ -23,14 +28,20 @@ export class AdtoComponent implements OnInit {
     {property: 'conta_contabil',      label: 'CTA.CONTABIL',type: 'string'},
     {property: 'natureza',            label: 'NATUREZA',    type: 'string'},
     {property: 'centro_custo',        label: 'C.CUSTO',     type: 'string'},
-    {property: 'banco_transf',        label: 'C.CUSTO',     type: 'string'},
-    {property: 'agencia_transf',      label: 'C.CUSTO',     type: 'string'},
-    {property: 'cta_corrente_transf', label: 'C.CUSTO',     type: 'string'},
+    {property: 'banco_transf',        label: 'BANCO',       type: 'string'},
+    {property: 'agencia_transf',      label: 'AGENCIA',     type: 'string'},
+    {property: 'cta_corrente_transf', label: 'C.CORRENTE',  type: 'string'},
   ];
 
+ 
+  itens: any  
+  
+  //constructor(private poNotification: PoNotificationService) {}
   constructor() {}
 
   ngOnInit(): void {
-    
   }
+
+    
+  
  }
